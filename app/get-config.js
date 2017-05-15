14a3deabf3b3690be391d910784e635f2250c773
Deven Vyas <deven.vyas@practo.com>
2017-05-15 15:23:20 +0530

    Throw error when it occurs. Change region. Change write file path

--- app/get-config.js
+++ app/get-config.js
@@ -1,14 +1,18 @@
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3({signatureVersion: 'v4', region: 'ap-southeast-1', apiVersion: '2012-11-05'});
+var s3 = new AWS.S3({signatureVersion: 'v4', region: 'ap-south-1', apiVersion: '2012-11-05'});
 
 var params = {
   Bucket: process.env.BUCKET_NAME,
   Key: process.env.CONFIG_PATH
 };
 
-var file = require('fs').createWriteStream('./config.js');
+var file = require('fs').createWriteStream('/app/config.js');
 
 s3
-  .getObject(params)
+  .getObject(params, function(err) {
+  })
   .createReadStream()
-  .pipe(file);
+  .pipe(file)
+  .on('error', function(error) {
+    if(error) throw (error);
+  })
